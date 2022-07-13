613e95842824d358f60b26c6989ce02f16ed8301
Manuel Zimmermann <manuelz.hh@gmail.com>
2022-07-13 13:30:09 +0200

    reupload project

--- app/src/main/java/com/tallence/tallencegame/model/GameState.kt
+++ app/src/main/java/com/tallence/tallencegame/model/GameState.kt
@@ -1,16 +1,14 @@
 package com.tallence.tallencegame.model
 
-class GameState() {
-    companion object {
-        var wins = 0
-        var losses = 0
-        var draws = 0
-        fun updateScore (gameResult: GameResult) {
-            when (gameResult) {
-                GameResult.WIN -> wins++
-                GameResult.LOSS -> losses++
-                else -> draws++
-            }
+object GameState {
+    var wins = 0
+    var losses = 0
+    var draws = 0
+    fun updateScore (gameResult: GameResult) {
+        when (gameResult) {
+            GameResult.WIN -> wins++
+            GameResult.LOSS -> losses++
+            else -> draws++
         }
     }
 }
